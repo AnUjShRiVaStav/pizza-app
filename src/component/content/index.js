59ef7e886e23db43a547742d0411f5a664b29922
ianujdev <anuj.s@intelora.co.in>
2021-12-17 19:21:32 +0530

    🚧 : Integrated Api

--- src/component/content/index.js
+++ src/component/content/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useEffect, useState } from 'react'
 import { styled } from '@mui/material/styles'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
@@ -11,9 +12,9 @@ import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import { red } from '@mui/material/colors'
 import ShoppingCartRounded from '@mui/icons-material/ShoppingCartRounded'
-import ShareIcon from '@mui/icons-material/Share'
+// import ShareIcon from '@mui/icons-material/Share'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
+// import MoreVertIcon from '@mui/icons-material/MoreVert'
 import AddIcon from '@mui/icons-material/Add'
 import RemoveIcon from '@mui/icons-material/Remove'
 import Button from '@mui/material/Button'
@@ -31,69 +32,84 @@ const ExpandMore = styled((props) => {
 
 export default function Content() {
   const [expanded, setExpanded] = React.useState(false)
-
+  const [loading, setLoading] = useState(false)
+  const [food, setFood] = useState([])
   const handleExpandClick = () => {
     setExpanded(!expanded)
   }
 
+  useEffect(() => {
+    fetch('https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68')
+      .then((response) => response.json())
+      .then((data) => setFood(data))
+  }, [])
+
   return (
-    <Card sx={{ maxWidth: 345 }} style={{ marginTop: '100px' }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            {/* <MoreVertIcon /> */}
-          </IconButton>
-        }
-        title="Name of Pizza"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        src="https://media.istockphoto.com/photos/cheesy-pepperoni-pizza-picture-id938742222?k=20&m=938742222&s=170667a&w=0&h=rnpCqZe2wx-4A6V8z91tAS_YRAJWlzySjKbU7f-1Nj8="
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          Description of Pizza
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <RemoveIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <AddIcon />
-        </IconButton>
-        {/* <IconButton> */}
-        <Button
-          variant="text"
-          style={{ color: 'gray' }}
-          startIcon={<ShoppingCartRounded />}
-        >
-          Add to Cart
-        </Button>
-        {/* </IconButton> */}
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Details</Typography>
-          <Typography paragraph>Rating Stars: ⭐⭐⭐⭐</Typography>
-          <Typography paragraph>Price: $250</Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    <div>
+      {food.map((val, index) => {
+        return (
+          <div>
+            <Card sx={{ maxWidth: 345 }} style={{ marginTop: '100px' }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                    R
+                  </Avatar>
+                }
+                action={
+                  <IconButton aria-label="settings">
+                    {/* <MoreVertIcon /> */}
+                  </IconButton>
+                }
+                title={val.name}
+              />
+              <CardMedia
+                component="img"
+                height="194"
+                src={val.img_url}
+                alt="Paella dish"
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {val.description}
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                  <RemoveIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                  <AddIcon />
+                </IconButton>
+                {/* <IconButton> */}
+                <Button
+                  variant="text"
+                  style={{ color: 'gray' }}
+                  startIcon={<ShoppingCartRounded />}
+                >
+                  Add to Cart
+                </Button>
+                {/* </IconButton> */}
+                <ExpandMore
+                  expand={expanded}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                  <ExpandMoreIcon />
+                </ExpandMore>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Typography paragraph>Details</Typography>
+                  <Typography paragraph>Rating Stars: {val.rating}</Typography>
+                  <Typography paragraph>Price: {val.price}</Typography>
+                </CardContent>
+              </Collapse>
+            </Card>
+          </div>
+        )
+      })}
+    </div>
   )
 }
--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -11,6 +11,7 @@ import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import Tooltip from '@mui/material/Tooltip'
 import MenuItem from '@mui/material/MenuItem'
+import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp'
 
 const pages = ['Products', 'Pricing', 'Blog']
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout']
@@ -116,6 +117,16 @@ const Navbar = () => {
                 />
               </IconButton>
             </Tooltip>
+            <ShoppingCartSharpIcon
+              style={{
+                marginLeft: '20px',
+                display: 'inline',
+                float: 'right',
+                width: '70px',
+                height: '40px',
+              }}
+            />
+
             <Menu
               sx={{ mt: '45px' }}
               id="menu-appbar"
