3a844fa46c8a5ed716f8f5389c31bc303ff85366
ianujdev <anuj.s@intelora.co.in>
2021-12-17 13:36:58 +0530

    💄 : Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import './App.css';
-
+import './App.css'
+import Navbar from './component/navbar'
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
